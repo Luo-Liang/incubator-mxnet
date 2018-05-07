96d0bd79c08d62c08237718996f97e317f6e5d0b
liangluo <liangluo@n37.sampa>
2018-05-07 17:41:57 +0000

    hierarchical sim support in MxNet

--- example/image-classification/run_local_debug.sh
+++ example/image-classification/run_local_debug.sh
@@ -2,7 +2,7 @@
 #$1=timeout
 #$2=net depth
 #$3=batch size
-ssh n45 -- redis-cli flushall
+#ssh n45 -- redis-cli flushall
 (pgrep python | xargs kill -9 && rm ~/train/profile.json) &>/dev/null
 (pgrep memcheck-amd64- | xargs kill -9) &> /dev/null
 export DISABLE_HEARTBEAT='TRUE'
@@ -14,11 +14,11 @@ verbose=$PS_VERBOSE
 #PS_VERBOSE=0
 #export DMLC_TRACKER_TOTAL_ID = 0
 export DMLC_PS_ROOT_PORT=9091; 
-export DMLC_NUM_WORKER=1;
+export DMLC_NUM_WORKER=2;
 export DMLC_NUM_SERVER=1;
 export DMLC_PS_ROOT_URI=127.0.0.1;
 export DMLC_ROLE=scheduler;  
-python train_mnist.py &
+python test_phub.py &
 
 
 #export PS_VERBOSE=0
@@ -26,34 +26,34 @@ export DMLC_TRACKER_TOTAL_ID=0
 #export PS_VERBOSE=2;
 export DMLC_ROLE=server; export DMLC_SERVER_ID=0
 #gdb --args python train_mnist.py --kv-store dist_sync_device 
-python train_mnist.py --kv-store dist_sync &
+#python train_mnist.py --kv-store dist_sync &
 #valgrind --error-limit=no python train_mnist.py --kv-store dist_sync_device &
 
 
 #export PS_VERBOSE=0
 export DMLC_TRACKER_TOTAL_ID=1
-export PS_VERBOSE=0;
+export PS_VERBOSE=2;
 export DMLC_ROLE=worker; export DMLC_WORKER_ID=0
 #valgrind --error-limit=no --vex-guest-max-insns=25 python train_mnist.py --kv-store dist_sync_device  --num-epochs 1 --gpus 0
 #gdb --args python train_mnist.py --kv-store dist_sync_device  --num-epochs 1 
 #gdb --args python train_mnist.py --kv-store dist_sync --num-epochs 1 --gpus 0
-#python train_mnist.py --kv-store dist_sync --num-epochs 1 &
+python test_phub.py --kv-store dist_sync_device --num-epochs 1 &
 
 
 export DMLC_TRACKER_TOTAL_ID=2
 export PS_VERBOSE=2
 export DMLC_ROLE=worker; export DMLC_WORKER_ID=1
 #valgrind  python train_mnist.py --kv-store dist_sync_device  --num-epochs 1 &
-gdb --args python train_mnist.py --kv-store dist_sync  --num-epochs 1 
-#python train_mnist.py --kv-store dist_sync_device --num-epochs 1 --gpus 0 &
-#python train_mnist.py --kv-store dist_sync_device --num-epochs 1  --gpus 0 &
+#gdb --args python train_mnist.py --kv-store dist_sync  --num-epochs 1 
+python test_phub.py --kv-store dist_sync_device --num-epochs 1 &
+
 
 
 
 #export PS_VERBOSE=0
 export DMLC_TRACKER_TOTAL_ID=3
-export PS_VERBOSE=0
+export PS_VERBOSE=2
 export DMLC_ROLE=server; export DMLC_SERVER_ID=1
-#gdb --args python train_mnist.py --kv-store dist_sync_device 
+gdb --args python test_phub.py --kv-store dist_sync_device 
 #python train_mnist.py --kv-store dist_sync_device --gpus 0 &
-#valgrind --error-limit=no python train_mnist.py --kv-store dist_sync_device &
+#valgrind --error-limit=no python test_phub.py --kv-store dist_sync_device &
--- example/image-classification/run_simulated.sh
+++ example/image-classification/run_simulated.sh
@@ -1,4 +1,4 @@
 ssh n45 -- redis-cli flushall
 sh stop.sh
 export MXNET_ENGINE_TYPE=ThreadedEnginePerDeviceLite
-~/mxnet/tools/launch.py -H hosts_simulated -n $4 -s $3 python train_imagenet.py  --benchmark 1 --network $1 --batch-size $2 --kv-store dist_sync_device   --image-shape 3,224,224 --disp-batches 50 $5 $6 $7 $8 $9 
+~/mxnet/tools/launch.py -H hosts_simulated -n $4 -s $3 python train_imagenet.py  --benchmark 1 --network $1 --batch-size $2 --kv-store dist_sync   --image-shape 3,224,224 --disp-batches 50 $5 $6 $7 $8 $9 
